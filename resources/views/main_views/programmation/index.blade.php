beea563cde5371440cd65d45828249804443b3d2
Jose Sicajan <sicajancoy@gmail.com>
2018-04-26 03:54:06 -0600

    Generic mia header

--- resources/views/main_views/programmation/index.blade.php
+++ resources/views/main_views/programmation/index.blade.php
@@ -1,16 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid mia-header">
-        <div class="row justify-content-start">
-            <div class="col-md-3 d-flex flex-column justify-content-center">
-                <div class="title-container border-bottom-white border-top-white">
-                    <h3 class="color-white display-2 font_3">Programacion</h3>
-                </div>
-            </div>
-            <div class="col-md-9"></div>
-        </div>
-    </div>
+    @include('elements.mia-hdear', ['title' => 'Programacion'])
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-8 col-md-offset-2">
