f97c6fa0cbcc4088dd2cca939fc3dbc944a970c1
Jose Sicajan <sicajancoy@gmail.com>
2018-04-26 04:44:55 -0600

    grids

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,48 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-8 col-md-offset-2">
+            <div class="col-12 col-md-6 p-4">
+                <div class="row flex-column">
+                    <div class="grid-header"></div>
+                    <div class="grid-block bg-grid-default"></div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 p-4">
+                <div class="row flex-column">
+                    <div class="grid-header"></div>
+                    <div class="grid-block bg-grid-default"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 p-4">
+                <div class="row flex-column">
+                    <div class="grid-header"></div>
+                    <div class="grid-block bg-grid-default"></div>
+                </div>
+
+            </div>
+            <div class="col-12 col-md-6 p-4">
+                <div class="row flex-column">
+                    <div class="grid-header"></div>
+                    <div class="grid-block bg-grid-default"></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 p-4">
+                <div class="row flex-column">
+                    <div class="grid-header"></div>
+                    <div class="grid-block bg-grid-default"></div>
+                </div>
+
+            </div>
+            <div class="col-12 col-md-6 p-4">
+                <div class="row flex-column">
+                    <div class="grid-header"></div>
+                    <div class="grid-block bg-grid-default"></div>
+                </div>
             </div>
         </div>
     </div>
